3adfff0f343306aae3cea881033fe147bac76036
Samuel Williams <samuelhwilliams@gmail.com>
2023-12-04 06:12:50 +0000

    Update samples/make-day

--- 2023/.templates/sample-day.py
+++ 2023/.templates/sample-day.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 
-def main(file_contents: str) -> int:
+from rich import print
+
+
+def part1(file_contents: str) -> int:
+    return 0
+
+
+def part2(file_contents: str) -> int:
     return 0
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        answer = main(f.read())
+        data = f.read()
 
-    print(f"The answer is {answer}")
+    answer1 = part1(data)
+    answer2 = part2(data)
+
+    print(f"The answer is: {answer1=}, {answer2=}")
--- 2023/make-day.py
+++ 2023/make-day.py
@@ -9,7 +9,7 @@ def make_day(day: str):
     dirname = f"day-{day}"
     os.mkdir(dirname)
     shutil.copyfile(".templates/sample-day.py", f"{dirname}/day.py")
-    shutil.copyfile(".templates/sample-test-day.py", f"{dirname}/test-day.py")
+    shutil.copyfile(".templates/sample-test-day.py", f"{dirname}/test_day.py")
 
 
 if __name__ == '__main__':
