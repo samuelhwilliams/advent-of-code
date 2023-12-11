4907837fadd322406c090d114406d551e5cd7014
Samuel Williams <samuelhwilliams@gmail.com>
2023-12-11 09:51:35 +0000

    Day 11 part 2 inc refactor

--- 2023/day_11.py
+++ 2023/day_11.py
@@ -2,44 +2,59 @@
 import itertools
 from typing import Iterable
 
-import numpy as np
+import pytest
 
 from rich import print
 
 from helpers import load_input, parse_grid
 
 
-def parse_file_contents(file_contents: str) -> np.array:
+def parse_file_contents(file_contents: str) -> list[list[str]]:
     grid = parse_grid(file_contents)
-    return np.array(grid)
-
-
-def expand_universe(grid: np.array) -> np.array:
-    for rotation in [1, -1]:
-        for dupe_row in [x for x, row in enumerate(grid) if all(cell == "." for cell in row)][::-1]:
-            grid = np.insert(grid, dupe_row, ".", axis=0)
-        grid = np.rot90(grid, k=rotation)
     return grid
 
 
-def find_star_coords(universe: np.array) -> list[tuple[int, int]]:
-    return [(x, y) for x in range(len(universe)) for y in range(len(universe[x])) if universe[x][y] == "#"]
+def yield_star_coords(universe: list[list[str]]) -> Iterable[list[tuple[int, int]]]:
+    yield from ((x, y) for x in range(len(universe)) for y in range(len(universe[x])) if universe[x][y] == "#")
 
 
 def get_star_pairs(star_coords: list[tuple[int, int]]) -> Iterable[tuple[tuple[int, int], tuple[int, int]]]:
     return itertools.combinations(star_coords, 2)
 
 
+def find_universal_expansions(universe) -> tuple[tuple[int], tuple[int]]:
+    x_expansions = tuple(x for x in range(len(universe)) if all(p == "." for p in universe[x]))
+    y_expansions = tuple(y for y in range(len(universe[0])) if all(universe[x][y] == "." for x in range(len(universe))))
+    return x_expansions, y_expansions
+
+
+def find_distances_between_stars(star_coords, x_expansions, y_expansions, expansion_factor):
+    for s1, s2 in get_star_pairs(star_coords):
+        yield abs(s1[0] - s2[0]) + abs(s1[1] - s2[1]) + (
+            (
+                len([x for x in x_expansions if min(s1[0], s2[0]) < x < max(s1[0], s2[0])])
+                + len([y for y in y_expansions if min(s1[1], s2[1]) < y < max(s1[1], s2[1])])
+            )
+            * (expansion_factor - 1)
+        )
+
+
 def part1(file_contents: str) -> int:
-    grid = parse_file_contents(file_contents)
-    universe = expand_universe(grid)
-    star_coords = find_star_coords(universe)
-    return sum(abs(s1[0] - s2[0]) + abs(s1[1] - s2[1]) for s1, s2 in get_star_pairs(star_coords))
+    universe = parse_file_contents(file_contents)
+    x_expansions, y_expansions = find_universal_expansions(universe)
+    return sum(
+        find_distances_between_stars(yield_star_coords(universe), x_expansions, y_expansions, expansion_factor=2)
+    )
 
 
-def part2(file_contents: str) -> int:
-    parse_file_contents(file_contents)
-    return 0
+def part2(file_contents: str, expansion_factor: int = 1_000_000) -> int:
+    universe = parse_file_contents(file_contents)
+    x_expansions, y_expansions = find_universal_expansions(universe)
+    return sum(
+        find_distances_between_stars(
+            yield_star_coords(universe), x_expansions, y_expansions, expansion_factor=expansion_factor
+        )
+    )
 
 
 if __name__ == "__main__":
@@ -71,5 +86,12 @@ def test_part1_real():
     assert part1(load_input(__file__)) == 10173804
 
 
-def test_part2():
-    assert part2(test_data) == 0
+@pytest.mark.parametrize(
+    "expansion_factor, expected_sum_distance",
+    (
+        (10, 1030),
+        (100, 8410),
+    ),
+)
+def test_part2(expansion_factor, expected_sum_distance):
+    assert part2(test_data, expansion_factor=expansion_factor) == expected_sum_distance
