4681ab43f8f31a0d0c5990a9e894ccdf04a4b984
Samuel Williams <samuelhwilliams@gmail.com>
2023-12-13 11:28:14 +0000

    Day 12 part 2 FINALLY

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import functools
 import re
 
 from rich import print
@@ -6,12 +7,18 @@ from rich import print
 from helpers import load_input
 
 
-def parse_file_contents(file_contents: str) -> list[list[str, list[int]]]:
-    data = [
-        [line.strip().split(" ")[0], list(map(int, line.strip().split(" ")[1].split(",")))]
-        for line in file_contents.strip().splitlines()
-    ]
-    return data
+def parse_file_contents(file_contents: str, part2: bool = False) -> tuple[tuple[str, tuple[int, ...]], ...]:
+    data = []
+
+    for line in file_contents.strip().splitlines():
+        report, spring_groups = line.strip().split(" ")  # type: str, str
+
+        if part2 is False:
+            data.append((report, tuple(map(int, spring_groups.split(",")))))
+        else:
+            data.append(("?".join([report] * 5), tuple(map(int, spring_groups.split(","))) * 5))
+
+    return tuple(data)
 
 
 def all_unknown_combinations(report: str):
@@ -27,7 +34,7 @@ def count_valid_groups(original_report, spring_groups):
     count = 0
 
     for i, report in enumerate(all_unknown_combinations(original_report)):
-        if [len(group) for group in filter(lambda x: x, re.split(r"\.+", report))] == spring_groups:
+        if tuple([len(group) for group in filter(lambda x: x, re.split(r"\.+", report))]) == spring_groups:
             count += 1
 
     return count
@@ -38,9 +45,29 @@ def part1(file_contents: str) -> int:
     return sum(count_valid_groups(report, spring_groups) for report, spring_groups in data)
 
 
+@functools.cache
+def recurse_reports(report, spring_groups):
+    if len(spring_groups) == 0:
+        return 0 if "#" in report else 1
+
+    total = 0
+    min_length_required = sum(spring_groups) + len(spring_groups) - 1
+    for i in range(0, len(report) - min_length_required + 1):
+        if i > 0 and report[i - 1] == "#":
+            break
+
+        if "." not in report[i : i + spring_groups[0]]:
+            if i + spring_groups[0] == len(report) and len(spring_groups) == 1:
+                total += 1
+            elif report[i + spring_groups[0]] != "#":
+                total += recurse_reports(report[i + spring_groups[0] + 1 :], spring_groups[1:])
+
+    return total
+
+
 def part2(file_contents: str) -> int:
-    parse_file_contents(file_contents)
-    return 0
+    data = parse_file_contents(file_contents, part2=True)
+    return sum(recurse_reports(report, spring_groups) for report, spring_groups in data)
 
 
 if __name__ == "__main__":
@@ -65,4 +92,4 @@ def test_part1():
 
 
 def test_part2():
-    assert part2(test_data) == 0
+    assert part2(test_data) == 525152
